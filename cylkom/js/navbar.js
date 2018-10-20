1a128691b5a9e665ce84a0f87eff28586506f0a4
Aviral Jain <aviralrocking@gmail.com>
2018-10-20 12:09:10 +0530

    scrollspy, appear animation, mailto

--- cylkom/js/navbar.js
+++ cylkom/js/navbar.js
@@ -2,7 +2,10 @@ $(document).ready(function() {
     // Transition effect for navbar 
     $(window).scroll(function() {
       // checks if window is scrolled more than 500px, adds/removes solid class
-    if($(this).scrollTop() > 200){
+    if($(this).scrollTop() > 500){
+        $('.services').addClass('appear-animation');
+    }
+    if($(this).scrollTop() > 120){
         $('.aboutUs').addClass('appear-animation');
     }
       if($(this).scrollTop() > 100) { 
